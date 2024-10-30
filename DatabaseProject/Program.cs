214f06d32ad5187ddfb09e532b6a7fca4efb5247
Kadir Halı <kadirhali07@gmail.com>
2024-10-30 15:49:53 +0300

    30.10.2024

--- DatabaseProject/Program.cs
+++ DatabaseProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DatabaseProject
@@ -14,7 +15,23 @@ namespace DatabaseProject
             Console.WriteLine("4-Çıkış Yap");
             tableNumber = Console.ReadLine();
 
-            SqlConnection connection = new SqlConnection("");
+            SqlConnection connection = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB; Initial Catalog=EgitimKampiDb; Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * from TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
     }
 }
